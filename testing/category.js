5a11bb18f2072767f9adf4a288274d965abaf5bb
Angeline Jacob <angeline.jacob.17@gmail.com>
2020-11-19 13:19:41 -0600

    added testing file

---
+++ testing/category.js
@@ -0,0 +1,57 @@
+{
+    "id": 14124,
+    "title": "sports by the numbers",
+    "clues_count": 5,
+    "clues": [
+        {
+            "id": 104795,
+            "answer": "football",
+            "question": "Fourth down",
+            "value": 200,
+            "airdate": "2011-02-23T12:00:00.000Z",
+            "category_id": 14124,
+            "game_id": null,
+            "invalid_count": null
+        },
+        {
+            "id": 104801,
+            "answer": "baseball",
+            "question": "Triple play",
+            "value": 400,
+            "airdate": "2011-02-23T12:00:00.000Z",
+            "category_id": 14124,
+            "game_id": null,
+            "invalid_count": null
+        },
+        {
+            "id": 104807,
+            "answer": "golf",
+            "question": "Triple bogey",
+            "value": 600,
+            "airdate": "2011-02-23T12:00:00.000Z",
+            "category_id": 14124,
+            "game_id": null,
+            "invalid_count": null
+        },
+        {
+            "id": 104813,
+            "answer": "tennis",
+            "question": "6-4, 7-6 (7-3)",
+            "value": 800,
+            "airdate": "2011-02-23T12:00:00.000Z",
+            "category_id": 14124,
+            "game_id": null,
+            "invalid_count": null
+        },
+        {
+            "id": 104819,
+            "answer": "boxing",
+            "question": "Standing 8 count",
+            "value": 1000,
+            "airdate": "2011-02-23T12:00:00.000Z",
+            "category_id": 14124,
+            "game_id": null,
+            "invalid_count": null
+        }
+    ]
+}
