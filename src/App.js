d1383730fdbddb2d280eb8f557fd7bab3bb5db30
joshuasuzuki <joshuakai.suzuki@gmail.com>
2020-11-19 13:59:36 -0500

    gameboard skeleton

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Gameboard from './gameboard/components/Gameboard'
 
 class App extends Component {
   constructor(props){
@@ -16,6 +17,7 @@ class App extends Component {
     return (
       <>
        <h1>Jeopardy</h1>
+       <Gameboard/>
       </>
     );
   }
---
+++ src/gameboard/components/Gameboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Gameboard extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+      <h3>Gameboard</h3>
+      </>
+    );
+  }
+}
+
+export default Gameboard;
