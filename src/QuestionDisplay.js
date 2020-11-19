b85271a643b2c99a9f9bba0636e5461d06da3196
Angeline Jacob <angeline.jacob.17@gmail.com>
2020-11-19 14:26:22 -0600

    added conditional rendering on Question Display

--- src/QuestionDisplay.js
+++ src/QuestionDisplay.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react'
 
 class QuestionDislay extends Component {
     render(){
+        let question = <p>Please select a question</p>
+        if(this.props.question){
+            question = <p>Question Display: {this.props.question}</p>
+        }
         return(
             <>
-            <p>Question Display: {this.props.question}</p>
+            {question}
             </>
         )
     }
