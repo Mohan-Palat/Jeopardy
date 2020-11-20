6e51b19f5bcfb9798dc084ad837791f540448d78
Angeline Jacob <angeline.jacob.17@gmail.com>
2020-11-20 14:04:03 -0600

    css changes to category header

--- src/gameboard/components/Clues.js
+++ src/gameboard/components/Clues.js
@@ -11,7 +11,7 @@ class Clues extends Component {
     })
     return (
       <>
-      <h3 className="clue">{this.props.title}</h3>
+      <h3 className="clue-category">{this.props.title}</h3>
       {clues}
       </>
     );
