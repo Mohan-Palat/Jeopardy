6bebb04c1760a54f267466ab828f59e8762730ae
joshuasuzuki <joshuakai.suzuki@gmail.com>
2020-11-20 01:40:07 -0500

    state array SOS

--- src/gameboard/components/Gameboard.js
+++ src/gameboard/components/Gameboard.js
@@ -22,58 +22,33 @@ class Gameboard extends Component {
    * @param {*} numbers Array of ID numbers
    */
   getCategoriesFromIDs = (numbers) =>{
-    let cats = []
     numbers.forEach((id)=>{
         this.getCategoryFromID(id)
         .then((response)=>{
-            //console.log(response.data)
+            console.log('axios data:',response.data)
 
-            cats.push(response.data)
+            this.setState(prevState=>({
+                categories:[...prevState.categories,response.data]
+            }));
         })
         .catch((error)=>{
             console.log(error);
         })
     });
 
-    //console.log(cats)
-    return cats;
   }
 
   componentDidMount() {
     // Get 6 random numbers and put them in array
     const categoryIDs = [14124];
 
-    const cats = this.getCategoriesFromIDs(categoryIDs);
-    // // For each category ID, get category data from axios
-    // // and make Category component
-    // const cats = categoryIDs.map((id)=>{
-    //     // for each ID call axios to get category
-    //     const category = {title:'',clues:[]};
-    //     // get category from axios and pass title and clues to Component
-    //     return <Category title={category.title}
-    //                      clues={category.clues}
-    //                      setClue={this.props.setClue}/>
-    // });
-
-    this.setState({
-        categories: {cats}
-    });
+    this.getCategoriesFromIDs(categoryIDs);
     
+    console.log('current state: ',this.state.categories);
   }
 
   render() {
 
-    //     // For each category ID, get category data from axios
-    // // and make Category component
-    // const categories = categoryIDs.map((id)=>{
-    //     // for each ID call axios to get category
-    //     const category = {title:'',clues:[]};
-    //     // get category from axios and pass title and clues to Component
-    //     return <Category title={category.title}
-    //                      clues={category.clues}
-    //                      setClue={this.props.setClue}/>
-    // })
-    //console.log(this.state.categories)
     let catsToRender;
     if(this.state.categories > 0){
          catsToRender = this.state.categories||[].map((category)=>{
