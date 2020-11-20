5b14123d4e114105a6cc42ba0bafdb7dea9c6916
Angeline Jacob <angeline.jacob.17@gmail.com>
2020-11-20 08:18:22 -0600

    fixed method name in inputform

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     super(props);
     this.state = {
       score:0,
-      currentClue: category.clues[0],
+      currentClue: {},
       userInput: '',
     }
   }
