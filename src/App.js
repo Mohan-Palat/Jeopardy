0b89fde31dc3f8baf022d8319628c18aef47d567
Angeline Jacob <angeline.jacob.17@gmail.com>
2020-11-24 14:42:45 -0600

    added background image

--- src/App.js
+++ src/App.js
@@ -47,8 +47,10 @@ class App extends Component {
        </div>
        <div id="main">
           <div className="game-board">
+            <div className='landing-buttons'>
               <Route path='/' exact component={() => <NewGame getCategoryIds={this.getCategoryIds}/>}/>
               <Route path='/' exact component={() => <Link to='/custom-settings'><button>Custom Game</button> </Link>}/>
+            </div>
 
               <Route path='/random' exact component={() => <Gameboard setClue={this.setClue} 
                                                                       idNums={this.state.categoryIds} 
